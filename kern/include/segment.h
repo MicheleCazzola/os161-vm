c5f80d68ca5991a305e9bf58fa5562884b109903
Michele Cazzola <michele.cazzola2407@gmail.com>
2024-08-03 21:45:35 +0200

    Added base segment files

---
+++ kern/include/segment.h
@@ -0,0 +1,9 @@
+/**
+ * Authors: Michele Cazzola, Leone Fabio, Filippo Forte - 2024
+ * Segments handling, used to distinguish among code, data, stack
+ */
+
+#ifndef _SEGMENT_H_
+#define _SEGMENT_H_
+
+#endif
---
+++ kern/vm/segment.c
@@ -0,0 +1,7 @@
+/**
+ * Authors: Michele Cazzola, Leone Fabio, Filippo Forte - 2024
+ * Segments handling, used to distinguish among code, data, stack
+ */
+
+#include <segment.h>
+#include "opt-paging.h"
