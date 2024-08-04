fd687f389c8809976e186d98185e6f28f19e87de
Michele Cazzola <michele.cazzola2407@gmail.com>
2024-08-04 23:22:27 +0200

    Added TLB abstraction TODO: decide whether to consider dirty bit or not

--- kern/include/vm_tlb.h
+++ kern/include/vm_tlb.h
@@ -6,4 +6,23 @@
 #ifndef _VM_TLB_H
 #define _VM_TLB_H
 
+/**
+ * VM_TLB_init: used to initialize TLB, needed at each process start and at bootstrap.
+ * 
+ * VM_TLB_get_victim_round_robin: executes replacement algorithm, returning victim index.
+ * Needed on TLB misses, when necessary to load a new entry into TLB.
+ * 
+ * VM_TLB_peek_victim: reads TLB victim entry, without executing replacement.
+ * Needed on TLB misses, when it is necessary to understand whether the selected entry
+ * was valid (actual replacement) or not (simple write).
+ * 
+ * VM_TLB_write: wrapper for TLB_write, it works with virtual and physical addresses
+ * instead of raw TLB entries.
+ */
+
+void vm_tlb_init(void);
+unsigned int vm_tlb_get_victim_round_robin(void);
+uint64_t vm_tlb_peek_victim(void);
+void vm_tlb_write(vaddr_t vaddr, paddr_t paddr,/* unsigned char dirty,*/ unsigned int index);
+
 #endif  /* _VM_TLB_H */
--- kern/vm/vm_tlb.c
+++ kern/vm/vm_tlb.c
@@ -4,6 +4,85 @@
  */
 
 #include <types.h>
+#include <vm.h>
 #include <mips/tlb.h>
 #include <vm_tlb.h>
 #include "opt-paging.h"
+
+static unsigned int current_victim;
+
+/**
+ * Invalidates all TLB entries
+ */
+static void vm_tlb_invalidate_entries() {
+    int i;
+
+    for(i = 0; i < NUM_TLB; i++) {
+        tlb_write(TLBHI_INVALID(i), TLBLO_INVALID(), i);
+    }
+}
+
+/**
+ * Initializes (or resets) replacement algorithm parameter
+ */
+static void vm_tlb_init_current_victim() {
+    current_victim = 0;
+}
+
+/**
+ * Initializes TLB, both for entry invalidation and
+ * round-robin replacement algorithm parameter 
+ */
+void vm_tlb_init() {
+    vm_tlb_invalidate_entries();
+    vm_tlb_init_current_victim();
+}
+
+/**
+ * Round-robin replacement algorithm execution: current victim is
+ * selected, and algorithm parameter is modified, following the policy used
+ */
+unsigned int vm_tlb_get_victim_round_robin() {
+
+    unsigned int victim;
+
+    victim = current_victim;
+    current_victim = (current_victim + 1) % NUM_TLB;
+
+    return victim;
+}
+
+/**
+ * Retrieves current victim without replacing it
+ */
+uint64_t vm_tlb_peek_victim() {
+    uint64_t victim_content = 0;
+    uint32_t entry_hi, entry_lo;
+
+    tlb_read(&entry_hi, &entry_lo, current_victim);
+
+    victim_content = (victim_content & entry_hi) << 32;
+    victim_content &= entry_lo;
+
+    return victim_content;
+}
+
+/**
+ * Writes a TLB entry into a specified position
+ * NOTE: to decide whether consider dirty bit handling or not
+ */
+void vm_tlb_write(vaddr_t vaddr, paddr_t paddr,/* unsigned char dirty,*/ unsigned int index) {
+
+    uint32_t entry_hi, entry_lo;
+
+    entry_hi = vaddr & PAGE_FRAME;
+    entry_lo = (paddr & TLBLO_VALID);
+
+    /*
+    if (dirty) {
+        entry_lo |= TLBLO_DIRTY;
+    }
+    */
+
+    tlb_write(entry_hi, entry_lo, index);
+}
